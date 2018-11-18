6f458667ba58fba4d69121aa0d45a70c8a551278
Nicolas Bouchard <boun0107@uqar.ca>
2018-11-18 17:33:12 -0500

    ajout des annotation XML

--- ServiceEnchere_NB/Objects/DemandeCreationEnchere.cs
+++ ServiceEnchere_NB/Objects/DemandeCreationEnchere.cs
@@ -5,22 +5,32 @@ using System.Web;
 
 namespace ServiceEnchere_NB.Objects
 {
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace = "http://BiztalkEnchereSchemas.DemandeCreationEnchere")]
+    [System.Xml.Serialization.XmlRoot(Namespace = "http://EAI.DemandeCreationEnchere", IsNullable = false)]
     public class DemandeCreationEnchere
     {
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string NomEnchere { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public decimal PrixMinimum { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string OptionTransport { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public int Duree { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string TypeEnchere { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string Categorie { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string MotsCles { get; set; }
 
+        [System.Xml.Serialization.XmlAttributeAttribute()]
         public string AutresInformations { get; set; }
     }
 }
